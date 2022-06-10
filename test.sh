f68b05c620496bd8ed36fa9b82d9ebc73415e5dd
Erdem Ersoy <erdemersoy256@gmail.com>
2022-06-10 09:49:11 +0300

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# DESCRIPTION: E93 OS testing script
+# AUTHOR: Erdem Ersoy (eersoy93)
+# COPYRIGHT: Copyright (c) 2022 Erdem Ersoy (eersoy93).
+# LICENSE: MIT license. See README.md file for deatils.
+
+qemu-system-i386 e93ldr.bin
