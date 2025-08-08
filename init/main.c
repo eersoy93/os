d1742fcda719b316f9cc2d9de774a6d30d6b4dad
Erdem Ersoy <erdemersoy256@gmail.com>
2025-08-08 22:11:36 +0300

    Remove unecessary include

--- init/main.c
+++ init/main.c
@@ -3,7 +3,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <sys/ioctl.h>
 #include "init.h"
 #include "gfx.h"
 #include "input.h"
@@ -71,12 +70,8 @@ int main(void)
                 r_big,
                 0xFF, 0x00, 0xFF);
 
-      // Optional: flush framebuffer mapping (usually not required)
-      // Many fbdev drivers update the display directly from the mmap'd buffer.
-      // A best-effort msync won't hurt; ignore failure as it's non-fatal on devices.
-        msync(state.fbp,
-            (size_t)state.vinfo.yres_virtual * state.finfo.line_length,
-            MS_SYNC);
+        // Flush the framebuffer to apply changes
+        FlushTheFramebuffer(state.fbp, &state.vinfo, &state.finfo);
 
         // Sleep for a while to avoid busy waiting
         sleep(1);
